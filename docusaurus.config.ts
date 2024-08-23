3919c3f73f7d2a7a11382fbacd0a8cb5db055193
Voary Nambinina <vnambs@gmail.com>
2024-08-23 12:12:37 +0300

    fix: docusaurus configuration :tada:

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,15 +8,15 @@ const config: Config = {
 	favicon: 'img/favicon.ico',
 
 	// Set the production url of your site here
-	url: 'https://your-docusaurus-site.example.com',
+	url: 'https://vnambs.github.io/docu',
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
 	baseUrl: '/docu/',
 
 	// GitHub pages deployment config.
 	// If you aren't using GitHub pages, you don't need these.
-	organizationName: 'facebook', // Usually your GitHub org/user name.
-	projectName: 'Documentation docusaurus', // Usually your repo name.
+	organizationName: 'Voary Nambinina', // Usually your GitHub org/user name.
+	projectName: 'Documentation for blogin system', // Usually your repo name.
 
 	onBrokenLinks: 'throw',
 	onBrokenMarkdownLinks: 'warn',
