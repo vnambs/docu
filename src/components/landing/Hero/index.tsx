cef05d5338457327d4b302710464a0c5dc31779c
Voary Nambinina <vnambs@gmail.com>
2024-08-28 17:53:53 +0300

    fix: updating structures of all the pages

--- src/components/landing/Hero/index.tsx
+++ src/components/landing/Hero/index.tsx
@@ -1,106 +1,92 @@
-import { type Variants, motion } from 'framer-motion';
+import { type Variants, motion } from 'framer-motion'
 
-import Translate from '@docusaurus/Translate';
+import Translate from '@docusaurus/Translate'
 
-import HeroSvg from './img/hero2.svg';
+import HeroSvg from './img/hero2.svg'
 
-import SocialLinks from '@site/src/components/SocialLinks';
-import { MovingButton } from '../../magicui/moving-border';
-import styles from './styles.module.css';
+import SocialLinks from '@site/src/components/SocialLinks'
+import { MovingButton } from '../../magicui/moving-border'
+import styles from './styles.module.css'
 
 const variants: Variants = {
-	visible: i => ({
-		opacity: 1,
-		y: 0,
-		transition: {
-			type: 'spring',
-			damping: 25,
-			stiffness: 100,
-			duration: 0.3,
-			delay: i * 0.3,
-		},
-	}),
-	hidden: { opacity: 0, y: 30 },
-};
+  visible: i => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: 'spring',
+      damping: 25,
+      stiffness: 100,
+      duration: 0.3,
+      delay: i * 0.3,
+    },
+  }),
+  hidden: { opacity: 0, y: 30 },
+}
 
 function Circle() {
-	return <div className={styles.circle} />;
+  return <div className={styles.circle} />
 }
 
 function Name() {
-	return (
-		<motion.div
-			className={styles.hero_text}
-			custom={1}
-			initial='hidden'
-			animate='visible'
-			variants={variants}
-			onMouseMove={e => {
-				e.currentTarget.style.setProperty('--x', `${e.clientX}px`);
-				e.currentTarget.style.setProperty('--y', `${e.clientY}px`);
-			}}>
-			<Translate id='homepage.hero.greet'>Bonjour! Je suis </Translate>
-			<span
-				className={styles.name}
-				onMouseMove={e => {
-					const bounding = e.currentTarget.getBoundingClientRect();
-					e.currentTarget.style.setProperty('--mouse-x', `${bounding.x}px`);
-					e.currentTarget.style.setProperty('--mouse-y', `${bounding.y}px`);
-				}}>
-				<Translate id='homepage.hero.name'>Voary Nambinina</Translate>
-			</span>
-			<span className='ml-1'>👋</span>
-		</motion.div>
-	);
+  return (
+    <motion.div
+      className={styles.hero_text}
+      custom={1}
+      initial="hidden"
+      animate="visible"
+      variants={variants}
+      onMouseMove={e => {
+        e.currentTarget.style.setProperty('--x', `${e.clientX}px`)
+        e.currentTarget.style.setProperty('--y', `${e.clientY}px`)
+      }}
+    >
+      <Translate id="homepage.hero.greet">Bonjour! Je suis </Translate>
+      <span
+        className={styles.name}
+        onMouseMove={e => {
+          const bounding = e.currentTarget.getBoundingClientRect()
+          e.currentTarget.style.setProperty('--mouse-x', `${bounding.x}px`)
+          e.currentTarget.style.setProperty('--mouse-y', `${bounding.y}px`)
+        }}
+      >
+        <Translate id="homepage.hero.name">Voary Nambinina</Translate>
+      </span>
+      <span className="ml-1">👋</span>
+    </motion.div>
+  )
 }
 
 export default function Hero() {
-	return (
-		<motion.div className={styles.hero}>
-			<div className={styles.intro}>
-				<Name />
-				<motion.p
-					custom={2}
-					initial='hidden'
-					animate='visible'
-					variants={variants}
-					className='max-lg:px-4'>
-					<Translate id='homepage.hero.text'>
-						Here, I'll be sharing the challenges encountered and solutions found
-						across various tech stacks, giving you insights into the latest
-						technologies and how to apply them in real-world development. I hope
-						my development experience can inspire you.
-					</Translate>
-				</motion.p>
-				<motion.div
-					custom={3}
-					initial='hidden'
-					animate='visible'
-					variants={variants}>
-					<SocialLinks />
-				</motion.div>
+  return (
+    <motion.div className={styles.hero}>
+      <div className={styles.intro}>
+        <Name />
+        <motion.p custom={2} initial="hidden" animate="visible" variants={variants} className="max-lg:px-4">
+          <Translate id="homepage.hero.text">
+            Here, I'll be sharing the challenges encountered and solutions found across various tech stacks, giving you
+            insights into the latest technologies and how to apply them in real-world development. I hope my development
+            experience can inspire you.
+          </Translate>
+        </motion.p>
+        <motion.div custom={3} initial="hidden" animate="visible" variants={variants}>
+          <SocialLinks />
+        </motion.div>
 
-				<motion.div
-					className='mt-4 flex gap-2'
-					custom={4}
-					initial='hidden'
-					animate='visible'
-					variants={variants}>
-					<MovingButton
-						borderRadius='1.25rem'
-						className='relative z-10 flex items-center rounded-2xl border border-neutral-200 border-solid bg-background px-5 py-3 text-center font-semibold text-base dark:border-neutral-800'>
-						<a
-							href={'/about'}
-							className='font-semibold'>
-							<Translate id='hompage.hero.introduce'>At a glance</Translate>
-						</a>
-					</MovingButton>
-				</motion.div>
-			</div>
-			<motion.div className={styles.background}>
-				<HeroSvg />
-				<Circle />
-			</motion.div>
-		</motion.div>
-	);
+        <motion.div className="mt-4 flex gap-2" custom={4} initial="hidden" animate="visible" variants={variants}>
+          <MovingButton
+            borderRadius="1.25rem"
+            className="relative z-10 flex items-center rounded-2xl border border-neutral-200 border-solid bg-background px-5 py-3 text-center font-semibold text-base dark:border-neutral-800"
+          >
+            <a href={'/about'} className="font-semibold">
+              <Translate id="hompage.hero.introduce">At a glance</Translate>
+            </a>
+          </MovingButton>
+        </motion.div>
+      </div>
+      <motion.div className={styles.background}>
+        <HeroSvg />
+        <Circle />
+      </motion.div>
+    </motion.div>
+  )
 }
