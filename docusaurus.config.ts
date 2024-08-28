17ae5c7a945478fc012e81039954a2b03879d772
Voary Nambinina <vnambs@gmail.com>
2024-08-28 09:02:48 +0300

    feat: starting new blog structures

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -39,18 +39,105 @@ const config: Config = {
 					editUrl:
 						'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
 				},
-				blog: {
-					showReadingTime: true,
-					// Please change this to your repo.
-					// Remove this to remove the "edit this page" links.
-					editUrl:
-						'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-				},
+				blog: false,
 				theme: {
 					customCss: './src/css/custom.css',
 				},
 			} satisfies Preset.Options,
 		],
+	],
+	plugins: [
+		'docusaurus-plugin-image-zoom',
+		'@docusaurus/plugin-ideal-image',
+		// ['docusaurus-plugin-baidu-tongji', { token: 'c9a3849aa75f9c4a4e65f846cd1a5155' }],
+		[
+			'@docusaurus/plugin-pwa',
+			{
+				debug: process.env.NODE_ENV === 'development',
+				offlineModeActivationStrategies: [
+					'appInstalled',
+					'standalone',
+					'queryString',
+				],
+				pwaHead: [
+					{ tagName: 'link', rel: 'icon', href: '/img/logo.png' },
+					{ tagName: 'link', rel: 'manifest', href: '/manifest.json' },
+					{ tagName: 'meta', name: 'theme-color', content: '#12affa' },
+				],
+			},
+		],
+		[
+			'vercel-analytics',
+			{
+				debug: process.env.NODE_ENV === 'development',
+				mode: 'auto',
+			},
+		],
+		[
+			'./src/plugin/plugin-content-blog', // 为了实现全局 blog 数据，必须改写 plugin-content-blog 插件
+			{
+				path: 'blog',
+				editUrl: ({ locale, blogDirPath, blogPath, permalink }) =>
+					`https://github.com/kuizuo/blog/edit/main/${blogDirPath}/${blogPath}`,
+				editLocalizedFiles: false,
+				blogDescription: '代码人生：编织技术与生活的博客之旅',
+				blogSidebarCount: 10,
+				blogSidebarTitle: 'Blogs',
+				postsPerPage: 12,
+				showReadingTime: true,
+				readingTime: ({ content, frontMatter, defaultReadingTime }) =>
+					defaultReadingTime({ content, options: { wordsPerMinute: 300 } }),
+				feedOptions: {
+					type: 'all',
+					title: '愧怍',
+					copyright: `Copyright © ${new Date().getFullYear()} 愧怍 Built with Docusaurus.<p><a href="http://beian.miit.gov.cn/" class="footer_lin">test</a></p>`,
+				},
+			},
+		],
+		async function tailwindcssPlugin() {
+			return {
+				name: 'docusaurus-tailwindcss',
+				configurePostCss(postcssOptions) {
+					// Appends TailwindCSS and AutoPrefixer.
+					postcssOptions.plugins.push(require('tailwindcss'));
+					postcssOptions.plugins.push(require('autoprefixer'));
+					return postcssOptions;
+				},
+			};
+		},
+		async function injectMotto() {
+			return {
+				name: 'docusaurus-motto',
+				injectHtmlTags() {
+					return {
+						headTags: [
+							{
+								tagName: 'script',
+								innerHTML: `
+    (${function () {
+			console.log(
+				`%c Kz Blog %c https://github.com/kuizuo/blog`,
+				'color: #fff; margin: 1em 0; padding: 5px 0; background: #12affa;',
+				'margin: 1em 0; padding: 5px 0; background: #efefef;'
+			);
+
+			const motto = `
+This Webisite Powered By Kz Blog.
+Written by Docusaurus, Coding with Love.
+--------
+Love what you do and do what you love.
+`;
+
+			if (document.firstChild?.nodeType !== Node.COMMENT_NODE) {
+				document.prepend(document.createComment(motto));
+			}
+		}.toString()})();`,
+							},
+						],
+					};
+				},
+			};
+		},
 	],
 
 	themeConfig: {
@@ -129,8 +216,8 @@ const config: Config = {
 			copyright: `Copyright © ${new Date().getFullYear()} Voary Nambinina, Inc. Built with Docusaurus.`,
 		},
 		prism: {
-			theme: prismThemes.github,
-			darkTheme: prismThemes.dracula,
+			theme: prismThemes.oneLight,
+			darkTheme: prismThemes.oneDark,
 		},
 	} satisfies Preset.ThemeConfig,
 };
--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,12 +1,12 @@
-import clsx from 'clsx';
-import Heading from '@theme/Heading';
-import styles from './styles.module.css';
+import Heading from '@theme/Heading'
+import clsx from 'clsx'
+import styles from './styles.module.css'
 
 type FeatureItem = {
-  title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
-  description: JSX.Element;
-};
+  title: string
+  Svg: React.ComponentType<React.ComponentProps<'svg'>>
+  description: JSX.Element
+}
 
 const FeatureList: FeatureItem[] = [
   {
@@ -14,8 +14,8 @@ const FeatureList: FeatureItem[] = [
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Docusaurus was designed from the ground up to be easily installed and used to get your website up and running
+        quickly.
       </>
     ),
   },
@@ -24,8 +24,8 @@ const FeatureList: FeatureItem[] = [
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go ahead and move your docs into the{' '}
+        <code>docs</code> directory.
       </>
     ),
   },
@@ -34,25 +34,25 @@ const FeatureList: FeatureItem[] = [
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Extend or customize your website layout by reusing React. Docusaurus can be extended while reusing the same
+        header and footer.
       </>
     ),
   },
-];
+]
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({ title, Svg, description }: FeatureItem) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
-      <div className="text--center padding-horiz--md">
+      <div className="padding-horiz--md text--center">
         <Heading as="h3">{title}</Heading>
         <p>{description}</p>
       </div>
     </div>
-  );
+  )
 }
 
 export default function HomepageFeatures(): JSX.Element {
@@ -66,5 +66,5 @@ export default function HomepageFeatures(): JSX.Element {
         </div>
       </div>
     </section>
-  );
+  )
 }
