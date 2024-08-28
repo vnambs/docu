ef9f6e4bbae33edf9850800a0f7265ed83ecec45
Voary Nambinina <vnambs@gmail.com>
2024-08-28 16:42:29 +0300

    fix: structures of the sites

--- data/features.tsx
+++ data/features.tsx
@@ -1,54 +1,84 @@
-import Translate, { translate } from '@docusaurus/Translate'
-import { Icon } from '@iconify/react'
-import OpenSourceSvg from '@site/static/svg/undraw_open_source.svg'
-import SpiderSvg from '@site/static/svg/undraw_spider.svg'
-import WebDeveloperSvg from '@site/static/svg/undraw_web_developer.svg'
+import Translate, { translate } from '@docusaurus/Translate';
+import { Icon } from '@iconify/react';
+import OpenSourceSvg from '@site/static/svg/undraw_open_source.svg';
+import SpiderSvg from '@site/static/svg/undraw_spider.svg';
+import WebDeveloperSvg from '@site/static/svg/web_developer.svg';
 
 export type FeatureItem = {
-  title: string | React.ReactNode
-  description: string | React.ReactNode
-  header: React.ReactNode
-  icon?: React.ReactNode
-}
+	title: string | React.ReactNode;
+	description: string | React.ReactNode;
+	header: React.ReactNode;
+	icon?: React.ReactNode;
+};
 
 const FEATURES: FeatureItem[] = [
-  {
-    title: translate({
-      id: 'homepage.feature.developer',
-      message: 'TypeScript 全栈工程师',
-    }),
-    description: (
-      <Translate>
-        作为一名 TypeScript 全栈工程师，秉着能用 TS 绝不用 JS 的原则，为项目提供类型安全的保障，提高代码质量和开发效率。
-      </Translate>
-    ),
-    header: <WebDeveloperSvg className={'h-auto w-full'} height={150} role="img" />,
-    icon: <Icon icon="logos:typescript-icon" className="h-4 w-4 text-neutral-500" />,
-  },
-  {
-    title: translate({
-      id: 'homepage.feature.spider',
-      message: '会点逆向 & 爬虫',
-    }),
-    description: (
-      <Translate>
-        作为一名曾学习与实践逆向工程两年半的开发者，对于逆向工程有着浓厚的兴趣，同时造就了超凡的阅读代码能力。没有看不懂的代码，只有不想看的代码。
-      </Translate>
-    ),
-    header: <SpiderSvg className={'h-auto w-full'} height={150} role="img" />,
-  },
-  {
-    title: translate({
-      id: 'homepage.feature.enthusiast',
-      message: '开源爱好者',
-    }),
-    description: (
-      <Translate>
-        作为一名开源爱好者，积极参与开源社区，为开源项目贡献代码，希望有生之年能够构建出一个知名的开源项目。
-      </Translate>
-    ),
-    header: <OpenSourceSvg className={'h-auto w-full'} height={150} role="img" />,
-  },
-]
+	{
+		title: translate({
+			id: 'homepage.feature.developer',
+			message: 'TypeScript Full-stack',
+		}),
+		description: (
+			<Translate>
+				As a TypeScript full-stack developer, I adhere to the principle of using
+				TS whenever possible instead of JS, ensuring type safety for projects,
+				improving code quality, and boosting development efficiency
+			</Translate>
+		),
+		header: (
+			<WebDeveloperSvg
+				className={'h-auto w-full'}
+				height={150}
+				role='img'
+			/>
+		),
+		icon: (
+			<Icon
+				icon='logos:typescript-icon'
+				className='h-4 w-4 text-neutral-500'
+			/>
+		),
+	},
+	{
+		title: translate({
+			id: 'homepage.feature.spider',
+			message: 'A bit of reverse engineering & web scraping.',
+		}),
+		description: (
+			<Translate>
+				As a developer who is full of curiosity in new technologies, I have a
+				strong interest in reverse engineering and have developed exceptional
+				code-reading skills. There is no code that I can't understand, only code
+				that I don't want to read
+			</Translate>
+		),
+		header: (
+			<SpiderSvg
+				className={'h-auto w-full'}
+				height={150}
+				role='img'
+			/>
+		),
+	},
+	{
+		title: translate({
+			id: 'homepage.feature.enthusiast',
+			message: 'Open-source enthusiast',
+		}),
+		description: (
+			<Translate>
+				As an open-source enthusiast, I actively participate in the open-source
+				community and contribute code to various projects. I hope to create a
+				well-known open-source project in my lifetime.
+			</Translate>
+		),
+		header: (
+			<OpenSourceSvg
+				className={'h-auto w-full'}
+				height={150}
+				role='img'
+			/>
+		),
+	},
+];
 
-export default FEATURES
+export default FEATURES;
